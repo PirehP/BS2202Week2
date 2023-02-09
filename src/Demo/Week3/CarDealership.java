52ca54b0d2995774f2a3b6a755a684d432a6de4c
Pireh Pirzada <pp62@st-andrews.ac.uk>
2023-02-09 10:53:41 +0000

    Added Car and its dealership

---
+++ src/Demo/Week3/CarDealership.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class CarDealership
+{
+    ArrayList<Car> listofCars;
+    public CarDealership()
+    {
+        listofCars = new  ArrayList<Car>();
+    }
+    
+}
